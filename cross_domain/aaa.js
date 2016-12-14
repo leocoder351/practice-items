ae7ec6cd1bdbb85bf940ac9faae53b19bd2ed805
liuxuan <liuxuan351@foxmail.com>
2016-12-14 23:10:59 +0800

    Add cross-domain

---
+++ cross_domain/aaa.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+   var pathname = __dirname + url.parse(req.url).pathname;
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile(pathname, function(err,data) {
+    res.end(data);
+  });
+}).listen(8000, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:8000/');
